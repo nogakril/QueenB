2b614baabe48762d777630839867b8750e4c7f5e
Gil Rosenberg <gil.rosenberg@mail.huji.ac.il>
2021-04-28 15:20:50 +0300

    meeting fragment update

--- app/src/main/java/queenb/app/y2021/queenb/ui/main/MeetingsFragment.java
+++ app/src/main/java/queenb/app/y2021/queenb/ui/main/MeetingsFragment.java
@@ -13,66 +13,66 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import queenb.app.y2021.queenb.MainScreenActivity;
 import queenb.app.y2021.queenb.R;
 
 
-public class MeetingsFragment extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class MeetingsFragment extends Fragment {
 
-    final int VIEW_TYPE_LOADING = 0;
-    final int VIEW_TYPE_ITEM = 1;
-    public List<String> meetings;
+//    final int VIEW_TYPE_LOADING = 0;
+//    final int VIEW_TYPE_ITEM = 1;
+//    public List<String> meetings;
 
-    public MeetingsFragment(List<String> meetings){
-        this.meetings = meetings;
+    public MeetingsFragment(){
     }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == VIEW_TYPE_ITEM) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
-            return new ItemViewHolder(view);
-        }
-        else {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
-            return new LoadingHolder(view);
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        populateItemRows((ItemViewHolder) holder, position);
-    }
-
-    @Override
-    public int getItemCount() {
-        return this.meetings == null ? 0 : this.meetings.size();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return this.meetings.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
-    }
-
-    private class ItemViewHolder extends RecyclerView.ViewHolder{
-        TextView tvItem;
-
-        public ItemViewHolder(@NonNull View itemView){
-            super(itemView);
-            tvItem = itemView.findViewById(R.id.tvItem);
-        }
-    }
-
-    private class LoadingHolder extends RecyclerView.ViewHolder{
-
-        public LoadingHolder(@NonNull View itemView) {
-            super(itemView);
-        }
-    }
-    private void populateItemRows(ItemViewHolder viewHolder, int position){
-        String item = this.meetings.get(position);
-        viewHolder.tvitem.setText(item);
-    }
+//
+//    @NonNull
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        if (viewType == VIEW_TYPE_ITEM) {
+//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+//            return new ItemViewHolder(view);
+//        }
+//        else {
+//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+//            return new LoadingHolder(view);
+//        }
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+//        populateItemRows((ItemViewHolder) holder, position);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return this.meetings == null ? 0 : this.meetings.size();
+//    }
+//
+//    @Override
+//    public int getItemViewType(int position) {
+//        return this.meetings.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+//    }
+//
+//    private class ItemViewHolder extends RecyclerView.ViewHolder{
+//        TextView tvItem;
+//
+//        public ItemViewHolder(@NonNull View itemView){
+//            super(itemView);
+//            tvItem = itemView.findViewById(R.id.tvItem);
+//        }
+//    }
+//
+//    private class LoadingHolder extends RecyclerView.ViewHolder{
+//
+//        public LoadingHolder(@NonNull View itemView) {
+//            super(itemView);
+//        }
+//    }
+//    private void populateItemRows(ItemViewHolder viewHolder, int position){
+//        String item = this.meetings.get(position);
+//        viewHolder.tvitem.setText(item);
+//    }
 
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container,
